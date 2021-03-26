526c3ad99bf2d37101ebb227ae37297f735d9079
jl24350 <ledesmajorge07@gmail.com>
2021-03-25 23:35:28 -0400

    added form data to app.js2

--- public/js/app.js
+++ public/js/app.js
@@ -9,16 +9,18 @@ const formData = new FormData();
 formData.append('name', 'this is a test')
 formData.append('email', 'ledesmajorge07@gmail.com')
 formData.append('password', 'conecone123')
+let x = {
+    name: 'jorge ledesma',
+    email: 'ledesmajorge07@gmail.com',
+    password: 'conecone123'
+}
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
-    fetch('https://jledesma-task-manager.herokuapp.com/users', {
+    fetch('/users', {
         method: 'POST',
-        body: {
-            "name" : 'jorge ledesma',
-            "email" : 'jledesma@eagles.bridgewater.edu',
-            "password": 'jljljl1231'
-        }
+        body: JSON.stringify(x),
+        headers: {'Content-Type': 'application/json'}
     }).then(res => res.json())
     .then(json => console.log(json));
 })
