80a57e2b1c161fc43a5507e431543567f572d152
jl24350 <ledesmajorge07@gmail.com>
2021-03-26 10:37:18 -0400

    added a create task page and functionality

--- public/js/createTask.js
+++ public/js/createTask.js
@@ -1,6 +1,28 @@
-const description = document.querySelector('input')
-const form = document.querySelector('form')
-const message = document.querySelector('#message')
+const { json } = require("express")
+
+const description = document.querySelector('#description')
+const form = document.querySelector('#form2')
+const message = document.querySelector('#message2')
+const form1 = document.querySelector('#from1')
+const email = document.querySelector('#email')
+const password = document.querySelector('password')
+
+
+form1.addEventListener('submit', (e) =>{
+
+    const x = {
+        email : email.value,
+        password :  password.value
+    }
+    fetch('/users/login', {
+        method: 'POST',
+        body: JSON.stringify(x),
+        headers:{'Content-Type': 'application/json'}
+    }).then(res => res.json()).then(json =>{
+        message.textContent = JSON.stringify(json)
+    })
+
+} )
 
 
 form.addEventListener('submit', (e) =>{
