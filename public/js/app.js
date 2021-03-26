b27d30184bb1d1da0a6daff6e9b3f0a7390f9050
jl24350 <ledesmajorge07@gmail.com>
2021-03-25 23:00:36 -0400

    added form data to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,15 +4,17 @@ const email = document.querySelector('#Email')
 const password = document.querySelector('#Password')
 const form = document.querySelector('form')
 const message = document.querySelector('#message')
+const formData = new FormData();
+
+formData.append('name', n.value)
+formData.append('email', email.value)
+formData.append('password', password.value)
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault()
-
     fetch('/users',{
         method: 'POST',
-        name: n.value,
-        email: email.value,
-        password: password.value
+        body: formData
     }).then((response) =>{
         response.json().then((data) =>{
             message.textContent = data
