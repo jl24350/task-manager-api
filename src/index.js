9341b28f0ff028476601c01562ab110f16d5d6f4
jl24350 <ledesmajorge07@gmail.com>
2021-03-25 21:45:44 -0400

    Added a static html

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use(express.static(publicDirectoryPath))
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
